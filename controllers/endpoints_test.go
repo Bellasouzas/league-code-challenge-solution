7ba5d107a9819fe76229bf12c0d28a28f09b9c37
isabsantos92@gmail.com <isabsantos92@gmail.com>
2020-09-07 22:49:22 -0300

    pass unit test Sum func

--- controllers/endpoints_test.go
+++ controllers/endpoints_test.go
@@ -43,3 +43,16 @@ func TestFlatten(t *testing.T) {
 		t.Errorf("The result is not correct, got %v, expected %v", got, want)
 	}
 }
+
+func TestSum(t *testing.T) {
+	records := [][]string{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
+	}
+	got := 45
+	want := controllers.Sum(records)
+	if got != want {
+		t.Errorf("The result is not correct, got %d, expected %d", got, want)
+	}
+}
